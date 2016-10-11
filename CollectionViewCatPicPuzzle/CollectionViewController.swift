0cb51bc8e407b7c6a0b442a93dd0086bfdde8f27
Ariela Cohen <arielacohen@Arielas-MacBook-Air.local>
2016-10-11 13:31:12 -0400

    almost finished

--- CollectionViewCatPicPuzzle/CollectionViewController.swift
+++ CollectionViewCatPicPuzzle/CollectionViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var headerReusableView: HeaderReusableView!
+    var footerReusableView: FooterReusableView!
+    var sectionInsets: UIEdgeInsets!
+    var spacing: CGFloat!
+    var itemSize: CGSize!
+    var referenceSize: CGSize!
+    var numberOfRows:CGFloat!
+    var numberOfColumns: CGFloat!
+    
+    func configureLayout() {
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        
+        CatPuzzleAttributes.defaultAttributes()
+        
+        numberOfRows = 4
+        numberOfColumns = 3
+        spacing = 2
+        sectionInsets = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        referenceSize = CGSize(width: screenWidth, height: 60)
+        let itemWidth = (screenWidth / numberOfColumns) - sectionInsets.left - sectionInsets.right - spacing
+        let itemHeight = (screenHeight / numberOfRows) - sectionInsets.top - sectionInsets.bottom - spacing - 30
+        itemSize = CGSize(width: itemWidth, height: itemHeight)
+        
+//        
+//      Using variables and calculations as needed, determine the item height. You will need to divide the screen height by the number of rows. You will also need to subtract an equal portion of the total amount of space being taken up by top/bottom insets, line spacing, and header/footer height.
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.collectionView?.register(HeaderReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
+        self.collectionView?.register(FooterReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "footer")
+        configureLayout()
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -28,6 +58,59 @@ class CollectionViewController: UICollectionViewController {
         return cell
         
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionElementKindSectionHeader {
+            
+            headerReusableView = (self.collectionView?.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)) as! HeaderReusableView
+            
+            return headerReusableView
+            
+        } else {
+            
+            footerReusableView = (self.collectionView?.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "footer", for: indexPath)) as! FooterReusableView
+            
+            return footerReusableView
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+}
+
+
+struct CatPuzzleAttributes {
+    
+    static func defaultAttributes() -> CatPuzzleAttributes {
+        
+        
+    }
+    
+    var height: Double
+    
     
     
 }
